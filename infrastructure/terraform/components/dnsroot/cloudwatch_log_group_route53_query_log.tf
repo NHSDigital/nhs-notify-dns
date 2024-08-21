ef6a079f15369c33b9f1d16ff91dfa02db08b22f
jamesthompson26-nhs <james.thompson26@nhs.net>
2024-08-21 14:06:20 +0100

    CCM-6245: TFSec Scanning Template Update

--- infrastructure/terraform/components/dnsroot/cloudwatch_log_group_route53_query_log.tf
+++ infrastructure/terraform/components/dnsroot/cloudwatch_log_group_route53_query_log.tf
@@ -9,7 +9,7 @@ resource "aws_cloudwatch_log_resource_policy" "route53_query_logging_policy" {
   provider = aws.us-east-1 # Route53 query logging must be in us-east-1 https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_query_log
 
   policy_document = data.aws_iam_policy_document.route53_logs.json
-  policy_name     = "${local.csi}-route53-query-logging-policy"
+  policy_name     = "${local.csi}-route53-query-logging-policy-blah"
 }
 
 data "aws_iam_policy_document" "route53_logs" {
