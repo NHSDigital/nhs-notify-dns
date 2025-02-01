1fd4c1b18cf51c30ae9070a36d28a774ccc22a9b
aidenvaines-bjss <54067008+aidenvaines-bjss@users.noreply.github.com>
2025-02-01 00:25:40 +0000

    Drift from template

--- scripts/terraform/terraform.mk
+++ scripts/terraform/terraform.mk
@@ -59,6 +59,8 @@ terraform-sec: # TFSEC check against Terraform files - optional: terraform_dir|d
 	tfsec infrastructure/terraform \
 		--force-all-dirs \
 		--exclude-downloaded-modules \
+		--tfvars-file infrastructure/terraform/etc/global.tfvars \
+		--tfvars-file infrastructure/terraform/etc/env_eu-west-2_main.tfvars \
 		--config-file scripts/config/tfsec.yaml
 
 # ==============================================================================
