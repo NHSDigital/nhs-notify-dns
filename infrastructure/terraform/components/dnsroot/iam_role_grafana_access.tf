06a543d6493e3a2941fdd4bae36f6ef3619a5a1b
sidnhs <siddharth.nair1@nhs.net>
2025-02-27 16:40:17 +0000

    CCM-8742: Grafana cross account access trust pol condition

--- infrastructure/terraform/components/dnsroot/iam_role_grafana_access.tf
+++ infrastructure/terraform/components/dnsroot/iam_role_grafana_access.tf
@@ -9,7 +9,17 @@ data "aws_iam_policy_document" "observability_grafana_role_assume_role_policy" {
     effect  = "Allow"
     principals {
       type        = "AWS"
-      identifiers = [replace("arn:aws:iam::${var.observability_account_id}:role/${local.csi}-grafana-workspace-role", "-${var.component}", "")]
+      identifiers = [
+         "arn:aws:iam::${var.observability_account_id}:root"
+      ]
+    }
+    condition {
+      test     = "ArnLike"
+      variable = "aws:PrincipalArn"
+
+      values = [
+        "arn:aws:iam::${var.observability_account_id}:role/*grafana-workspace-role"
+      ]
     }
   }
 }
